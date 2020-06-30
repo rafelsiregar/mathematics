538a5d6d837e2a69fd2043bfee4089dcaf23d8cd
Rafel Siregar <50065143+rafelsiregar@users.noreply.github.com>
2020-06-30 15:53:25 +0700

    Create utopiantree.java

---
+++ utopiantree.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the utopianTree function below.
+    //Time Complexity : O(n)
+    static int utopianTree(int n) {
+            if(n==0) return 1;
+            else {
+                int i = 1;
+                int count = 1;
+                while(i++<=n){
+                    if(i%2!=0) count +=1;
+                    else count *=2;
+                }
+                return count;
+            }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int result = utopianTree(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
